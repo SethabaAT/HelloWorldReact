4b4c44f403e927a5666b4630eb68df2ef00e24a5
Arnold Thabo Sethaba <80105049+SethabaAT@users.noreply.github.com>
2023-07-20 15:49:27 +0200

    CSS with React

--- src/App.js
+++ src/App.js
@@ -9,11 +9,18 @@ import EventBind from "./components/EventBind";
 import UserGreeting from "./components/UserGreeting";
 import ParentComponent from "./components/ParentComponent";
 import NameList from "./components/NameList";
+import Stylesheet from "./components/Stylesheet";
+import "./appStyles.css";
+import styles from "./appStyles.module.css";
 
 function App() {
   return (
     <div className="App">
-      <NameList />
+      <h1 className="error">Error</h1>
+      <h1 className={styles.success}>Success</h1>
+
+      {/* <Stylesheet primary={true} /> */}
+      {/* <NameList /> */}
       {/* <UserGreeting /> */}
       {/* <EventBind /> */}
       {/* <FunctionClick /> */}
