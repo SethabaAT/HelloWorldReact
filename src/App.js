be19c0151421f0ec7225d9313afe2ca214f1fd92
Arnold Thabo Sethaba <80105049+SethabaAT@users.noreply.github.com>
2023-07-20 14:01:34 +0200

    Modified the user greetings to have more select methods

--- src/App.js
+++ src/App.js
@@ -6,21 +6,13 @@ import Counter from "./components/Counter";
 import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
-<<<<<<< HEAD
 import UserGreeting from "./components/UserGreeting";
-=======
 import ParentComponent from "./components/ParentComponent";
->>>>>>> 53c1596733139a540b1ebe007630150ce1555b34
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <UserGreeting></UserGreeting>
-      
-=======
-      <ParentComponent></ParentComponent>
->>>>>>> 53c1596733139a540b1ebe007630150ce1555b34
+      <UserGreeting />
       {/* <EventBind /> */}
       {/* <FunctionClick /> */}
       {/* <ClassClick /> */}
--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -8,13 +8,34 @@ class UserGreeting extends Component {
       isLogin: false,
     };
   }
-
+  // There are different ways to do selection in React
+  // 1. If statement else statement
+  // 2. Note that you can even make a tag a variable
+  // 3. We can also use itenary operator
+  // 4. Short Circuit operator
   render() {
-    if (this.state.isLogin) {
-      return <div>Hello Thabo</div>;
-    } else {
-      return <div> Welcome guest</div>;
-    }
+    // Using the itenary operator
+    return this.state.isLogin ? (
+      <div>Hello Thabo</div>
+    ) : (
+      <div> Welcome guest</div>
+    );
+
+    // Using the message variable
+    // let message;
+    // if (this.state.isLogin) {
+    //   message = <div>Hello Thabo</div>;
+    // } else {
+    //   message = <div> Welcome guest</div>;
+    // }
+
+    //
+    //return <div>{message}</div>;
+    // if (this.state.isLogin) {
+    //   return <div>Hello Thabo</div>;
+    // } else {
+    //   return <div> Welcome guest</div>;
+    // }
   }
 }
 
