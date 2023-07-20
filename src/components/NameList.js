b0759d226609df358502a17a9ba542353e6c7f65
Arnold Thabo Sethaba <80105049+SethabaAT@users.noreply.github.com>
2023-07-20 15:18:35 +0200

    Modified the name list to inlcude keys

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,30 +1,34 @@
 import React from "react";
 import Person from "./Person";
 
-// function NameList() {
-//   // An array of names
-//   const names = ["Bruce", "Barry", "Arthur", "Diana"];
-
-//   // Transform names from strings to jsx
-//   let nameList = names.map((n) => <h2>{n}</h2>);
-
-//   // Return the list of objects
-//   return <div>{nameList}</div>;
-// }
-
 function NameList() {
   // An array of names
-  const persons = [
-    { id: 1, name: "Bruce", age: 28, skill: "React" },
-    { id: 2, name: "Clark", age: 36, skill: "Angular" },
-    { id: 2, name: "Clark", age: 36, skill: "Vue" },
-  ];
+  const names = ["Bruce", "Barry", "Arthur", "Diana"];
 
   // Transform names from strings to jsx
-  let personList = persons.map((person) => <Person person={person} />);
+  // Note you need a unique key value. You can use an index
+  let nameList = names.map((name, index) => <h2 key={index}>{name}</h2>);
 
   // Return the list of objects
-  return <div>{personList}</div>;
+  return <div>{nameList}</div>;
 }
+
+// function NameList() {
+//   // An array of names
+//   const persons = [
+//     { id: 1, name: "Bruce", age: 28, skill: "React" },
+//     { id: 2, name: "Clark", age: 36, skill: "Angular" },
+//     { id: 3, name: "Clark", age: 36, skill: "Vue" },
+//   ];
+
+//   // Transform names from strings to jsx
+//   // Note that whenever you have a list. React needs you to add keys
+//   let personList = persons.map((person) => (
+//     <Person key={person.id} person={person} />
+//   ));
+
+//   // Return the list of objects
+//   return <div>{personList}</div>;
+// }
 
 export default NameList;
