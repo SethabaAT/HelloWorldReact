9f038d4d4da19a7078b8002a48689e72a0d04417
Arnold Thabo Sethaba <80105049+SethabaAT@users.noreply.github.com>
2023-07-24 13:52:13 +0200

    Added an example where Fragment is used. Table and Colummns

--- src/Columns.js
+++ src/Columns.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 function Columns() {
+  const items = []; // A variable
   // Note that td does not allow to be inside a div tag
   //Therefore we can use a react fragment
+  // You can use <> instead of react fragment
   return (
-    <React.Fragment>
+    <>
       <td>Name</td>
       <td>Sethaba</td>
-    </React.Fragment>
+    </>
   );
 }
 
